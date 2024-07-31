cd26e91e7a59fabbcc568e82c6a91b8b59396e99
Harendrasinh <haren111990@gmail.com>
2024-07-31 15:55:45 +0530

    web name

--- super.js
+++ super.js
@@ -3,6 +3,6 @@ const btn=document.querySelector("#btn");
 const msg=document.querySelector("#under-con");
 btn.addEventListener("click",()=>{
     if(userInput){
-        msg.innerHTML="Site Under Constuction";
+        msg.innerHTML="Sorry Site Under Constuction, Please contact Harendrasinh on haren111990@VayuSoftwares.com";
     }
 });
