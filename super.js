0afce6b76e44f1e883e163e3b028d4b85a4d36d5
Harendrasinh <haren111990@gmail.com>
2024-08-01 23:07:23 +0530

    change

--- super.js
+++ super.js
@@ -1,6 +1,5 @@
 const userInput=document.querySelector("#user-input");
 const btn=document.querySelector("#btn");
-const msg=document.querySelector("#under-con");
 const imgs=document.querySelector("#images");
 
 
