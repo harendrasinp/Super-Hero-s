a71552d4c577f7d2cf0a041ba6ec02ae91add299
Harendrasinh <haren111990@gmail.com>
2024-08-02 14:01:27 +0530

    change all GUI

--- super.js
+++ super.js
@@ -1,12 +1,14 @@
 const userInput=document.querySelector("#user-input");
 const btn=document.querySelector("#btn");
-const imgs=document.querySelector("#images");
-
+const imgesection=document.querySelector("#images");
 
 userInput.addEventListener("input",()=>callServer(userInput.value));
-btn.addEventListener("click",callServer);
+btn.addEventListener("click",()=>{
+  callServer(userInput.value)
+});
+
 async function callServer(hname){
-    imgs.innerHTML=``;
+  imgesection.innerHTML=``;
     console.log(hname);
     const heroName= await fetch(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${hname}&apikey=9ab871748d83ae2eb5527ffd69e034de&hash=d35377547e551cd64a60657d2517bb7f?ts=1`)
     let jsonName=await heroName.json();
@@ -17,31 +19,29 @@ async function callServer(hname){
 function loopfunction(heros){
   heros.forEach(element => {
       console.log(element);
-      const heroName=element["name"];
       const im=element["thumbnail"].path;
       const ext=element["thumbnail"].extension;
+      const heroId=element["id"];
+      const heroName=element["name"];
+      const heroComics=element["comics"].available;
 
-    // --------------main-div----------------------------------
-      const mainDiv=document.createElement("div");
-      mainDiv.classList.add("main-div");
-    // --------------image-div---------------------------------
-      const imgDiv=document.createElement("div");
-      imgDiv.classList.add("img-div");
-      imgDiv.innerHTML=`<img src="${im}.${ext}" alt="">`
-    // --------------name-div---------------------------------
-    const namDiv=document.createElement("div");
-    namDiv.classList.add("name-div");
-    namDiv.innerHTML=`Name: ${heroName}`;
-    // 
-    // -----------------fevourit-button-----------------------
-    const favBtn=document.createElement("button");
-    // ----------------------------------------------------------
-      mainDiv.append(imgDiv);
-      imgs.append(mainDiv);
-      mainDiv.append(namDiv)
-      mainDiv.append(favBtn)
-      
+      const mainFram=document.createElement("div");
+      mainFram.classList.add("main-fram");
+
+      mainFram.innerHTML=`<img src="${im}.${ext}" alt="" id="img-fram">
+                                            <div id="detail-div">
+                                              <div class="h-id">Id: ${heroId}</div>
+                                              <div class="h-name">Name: ${nameSize(heroName)}</div>
+                                              <div class="h-comics">Comics: ${heroComics}</div>
+                                            </div>
+                                                <div id="btn-div">
+                                                     <button type="submit" class="add-btn">Add to Favourite</button>
+                                                </div>`
+      imgesection.append(mainFram)
   });
+}
+function nameSize(name){
+     return result=name.substring(0,12)
 }
         
 
